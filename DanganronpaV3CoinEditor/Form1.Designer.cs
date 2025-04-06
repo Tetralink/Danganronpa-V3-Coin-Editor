e87babe70455b1621192a9878579da6259d8f59a
Tetralink <116966890+Tetralink@users.noreply.github.com>
2025-04-06 20:23:09 +0200

    Add files via upload
    The code

---
+++ DanganronpaV3CoinEditor/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace DanganronpaV3CoinEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.OriginalSavePath = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.OutputSavePath = new System.Windows.Forms.Label();
+            this.txtOutputPath = new System.Windows.Forms.TextBox();
+            this.CoinAmount = new System.Windows.Forms.Label();
+            this.txtCoinAmount = new System.Windows.Forms.TextBox();
+            this.chkBackup = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CreateLabel = new System.Windows.Forms.Label();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CoinsCasinoEditor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.Location = new System.Drawing.Point(285, 75);
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.Size = new System.Drawing.Size(378, 20);
+            this.txtSavePath.TabIndex = 0;
+            // 
+            // OriginalSavePath
+            // 
+            this.OriginalSavePath.AutoSize = true;
+            this.OriginalSavePath.Font = new System.Drawing.Font("DorBlue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OriginalSavePath.Location = new System.Drawing.Point(20, 70);
+            this.OriginalSavePath.Name = "OriginalSavePath";
+            this.OriginalSavePath.Size = new System.Drawing.Size(259, 26);
+            this.OriginalSavePath.TabIndex = 1;
+            this.OriginalSavePath.Text = "Original Save Path";
+            this.OriginalSavePath.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("DorBlue", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(670, 74);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // OutputSavePath
+            // 
+            this.OutputSavePath.AutoSize = true;
+            this.OutputSavePath.Font = new System.Drawing.Font("DorBlue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputSavePath.Location = new System.Drawing.Point(20, 112);
+            this.OutputSavePath.Name = "OutputSavePath";
+            this.OutputSavePath.Size = new System.Drawing.Size(247, 26);
+            this.OutputSavePath.TabIndex = 4;
+            this.OutputSavePath.Text = "Output Save Path";
+            // 
+            // txtOutputPath
+            // 
+            this.txtOutputPath.Location = new System.Drawing.Point(273, 118);
+            this.txtOutputPath.Name = "txtOutputPath";
+            this.txtOutputPath.Size = new System.Drawing.Size(390, 20);
+            this.txtOutputPath.TabIndex = 3;
+            // 
+            // CoinAmount
+            // 
+            this.CoinAmount.AutoSize = true;
+            this.CoinAmount.Font = new System.Drawing.Font("DorBlue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CoinAmount.Location = new System.Drawing.Point(20, 154);
+            this.CoinAmount.Name = "CoinAmount";
+            this.CoinAmount.Size = new System.Drawing.Size(170, 26);
+            this.CoinAmount.TabIndex = 7;
+            this.CoinAmount.Text = "Coin Amount";
+            // 
+            // txtCoinAmount
+            // 
+            this.txtCoinAmount.Font = new System.Drawing.Font("DorBlue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCoinAmount.Location = new System.Drawing.Point(196, 159);
+            this.txtCoinAmount.Name = "txtCoinAmount";
+            this.txtCoinAmount.Size = new System.Drawing.Size(71, 24);
+            this.txtCoinAmount.TabIndex = 6;
+            this.txtCoinAmount.Text = "999999";
+            this.txtCoinAmount.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // chkBackup
+            // 
+            this.chkBackup.AutoSize = true;
+            this.chkBackup.Font = new System.Drawing.Font("DorBlue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBackup.Location = new System.Drawing.Point(285, 154);
+            this.chkBackup.Name = "chkBackup";
+            this.chkBackup.Size = new System.Drawing.Size(229, 30);
+            this.chkBackup.TabIndex = 8;
+            this.chkBackup.Text = "Create backup";
+            this.chkBackup.UseVisualStyleBackColor = true;
+            this.chkBackup.CheckedChanged += new System.EventHandler(this.chkBackup_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("DorBlue", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(670, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Create";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CreateLabel
+            // 
+            this.CreateLabel.AutoSize = true;
+            this.CreateLabel.Font = new System.Drawing.Font("DorBlue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateLabel.Location = new System.Drawing.Point(543, 155);
+            this.CreateLabel.Name = "CreateLabel";
+            this.CreateLabel.Size = new System.Drawing.Size(121, 26);
+            this.CreateLabel.TabIndex = 10;
+            this.CreateLabel.Text = "Create :";
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Font = new System.Drawing.Font("DorBlue", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseOutput.Location = new System.Drawing.Point(670, 118);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOutput.TabIndex = 11;
+            this.btnBrowseOutput.Text = "Browse";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Image = global::DanganronpaV3CoinEditor.Properties.Resources.DanganronpaV3_blacklogo;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CoinsCasinoEditor
+            // 
+            this.CoinsCasinoEditor.AutoSize = true;
+            this.CoinsCasinoEditor.Font = new System.Drawing.Font("DorBlue", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CoinsCasinoEditor.Location = new System.Drawing.Point(270, 21);
+            this.CoinsCasinoEditor.Name = "CoinsCasinoEditor";
+            this.CoinsCasinoEditor.Size = new System.Drawing.Size(475, 46);
+            this.CoinsCasinoEditor.TabIndex = 13;
+            this.CoinsCasinoEditor.Text = "Coins Casino Editor";
+            this.CoinsCasinoEditor.Click += new System.EventHandler(this.CoinsCasinoEditor_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 203);
+            this.Controls.Add(this.CoinsCasinoEditor);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.CreateLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chkBackup);
+            this.Controls.Add(this.CoinAmount);
+            this.Controls.Add(this.txtCoinAmount);
+            this.Controls.Add(this.OutputSavePath);
+            this.Controls.Add(this.txtOutputPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.OriginalSavePath);
+            this.Controls.Add(this.txtSavePath);
+            this.Name = "Form1";
+            this.Text = "DanganronpaVCoinEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Label OriginalSavePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label OutputSavePath;
+        private System.Windows.Forms.TextBox txtOutputPath;
+        private System.Windows.Forms.Button btnBrowseOutput_Click;
+        private System.Windows.Forms.Label CoinAmount;
+        private System.Windows.Forms.TextBox txtCoinAmount;
+        private System.Windows.Forms.CheckBox chkBackup;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label CreateLabel;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label CoinsCasinoEditor;
+    }
+}
+
---
+++ DanganronpaV3CoinEditor/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DanganronpaV3CoinEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkBackup_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Save files (*.dat)|*.dat|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSavePath.Text = openFileDialog.FileName;
+            }
+        }
+        private void btnBrowseOutput_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Save files (*.dat)|*.dat|All files (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputPath.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sourcePath = txtSavePath.Text;
+            string outputPath = txtOutputPath.Text;
+            string coinText = txtCoinAmount.Text;
+
+            // Vérification des champs
+            if (string.IsNullOrWhiteSpace(sourcePath) ||
+                string.IsNullOrWhiteSpace(outputPath) ||
+                string.IsNullOrWhiteSpace(coinText))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            // Vérifie si le nombre est valide
+            if (!int.TryParse(coinText, out int coinAmount) || coinAmount < 0 || coinAmount > 999999999)
+            {
+                MessageBox.Show("Please enter a valid number of coins (0 - 999999999).");
+                return;
+            }
+
+            try
+            {
+                // Création d'une backup si demandé
+                if (chkBackup.Checked)
+                {
+                    string backupPath = sourcePath + ".bak";
+                    File.Copy(sourcePath, backupPath, true);
+                }
+
+                // Lit le fichier de sauvegarde
+                byte[] saveData = File.ReadAllBytes(sourcePath);
+
+                // Trouve l'offset et la taille de la valeur des pièces (À MODIFIER SELON LA STRUCTURE DU FICHIER)
+                int coinOffset = 0xB458;
+                int coinSize = 4; // Remplacez par la taille correcte (généralement 4 bytes pour un entier)
+
+                // Convertit le nombre de pièces en tableau d'octets (Big Endian)
+                byte[] newCoinAmount = BitConverter.GetBytes(coinAmount);
+
+                // Copie les nouveaux octets dans le tableau de sauvegarde
+                Array.Copy(newCoinAmount, 0, saveData, coinOffset, coinSize);
+
+                // Écrit les données modifiées dans le fichier de sortie
+                File.WriteAllBytes(outputPath, saveData);
+
+                MessageBox.Show("Save modified!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Une erreur s'est produite : " + ex.Message);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CoinsCasinoEditor_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
